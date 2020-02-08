fd659ebf1629a9fba20713c218b0671ec0c9dab4
Janith Ganewatta <janith@upventrix.com>
2020-02-08 11:30:27 +0300

    Initial Commit

---
+++ sachithsilva-cobsccomp182p-018Tests/sachithsilva_cobsccomp182p_018Tests.swift
@@ -0,0 +1,34 @@
+//
+//  sachithsilva_cobsccomp182p_018Tests.swift
+//  sachithsilva-cobsccomp182p-018Tests
+//
+//  Created by Janith Ganewatta on 2/8/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import XCTest
+@testable import sachithsilva_cobsccomp182p_018
+
+class sachithsilva_cobsccomp182p_018Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
